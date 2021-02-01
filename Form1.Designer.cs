1025d0e1c6e18c6c2d79b346edef4ab281d76301
James Keith Buchanan <jamesbuchanan007@gmail.com>
2021-02-01 07:47:08 -0600

    Initial

---
+++ Form1.Designer.cs
@@ -0,0 +1,268 @@
+﻿
+namespace ESO_Merchant
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.listBxItems = new System.Windows.Forms.ListBox();
+			this.picBoxItemCost = new System.Windows.Forms.PictureBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.picBoxItemPrice = new System.Windows.Forms.PictureBox();
+			this.txtCost = new System.Windows.Forms.TextBox();
+			this.txtPrice = new System.Windows.Forms.TextBox();
+			this.label5 = new System.Windows.Forms.Label();
+			this.label6 = new System.Windows.Forms.Label();
+			this.ckBoxIsListed = new System.Windows.Forms.CheckBox();
+			this.btnAdd = new System.Windows.Forms.Button();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.btnSearch = new System.Windows.Forms.Button();
+			this.comboBox1 = new System.Windows.Forms.ComboBox();
+			this.btnAll = new System.Windows.Forms.Button();
+			this.btnNotListed = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.picBoxItemCost)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.picBoxItemPrice)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// listBxItems
+			// 
+			this.listBxItems.FormattingEnabled = true;
+			this.listBxItems.Location = new System.Drawing.Point(8, 96);
+			this.listBxItems.Name = "listBxItems";
+			this.listBxItems.Size = new System.Drawing.Size(308, 212);
+			this.listBxItems.TabIndex = 0;
+			// 
+			// picBoxItemCost
+			// 
+			this.picBoxItemCost.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.picBoxItemCost.Location = new System.Drawing.Point(325, 182);
+			this.picBoxItemCost.Name = "picBoxItemCost";
+			this.picBoxItemCost.Size = new System.Drawing.Size(342, 50);
+			this.picBoxItemCost.TabIndex = 2;
+			this.picBoxItemCost.TabStop = false;
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.ForeColor = System.Drawing.Color.White;
+			this.label2.Location = new System.Drawing.Point(322, 166);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(141, 13);
+			this.label2.TabIndex = 3;
+			this.label2.Text = "Image - Item Name and Cost";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.ForeColor = System.Drawing.Color.White;
+			this.label3.Location = new System.Drawing.Point(322, 235);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(125, 13);
+			this.label3.TabIndex = 5;
+			this.label3.Text = "Image - Item Future Price";
+			// 
+			// picBoxItemPrice
+			// 
+			this.picBoxItemPrice.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.picBoxItemPrice.Location = new System.Drawing.Point(325, 251);
+			this.picBoxItemPrice.Name = "picBoxItemPrice";
+			this.picBoxItemPrice.Size = new System.Drawing.Size(342, 50);
+			this.picBoxItemPrice.TabIndex = 4;
+			this.picBoxItemPrice.TabStop = false;
+			// 
+			// txtCost
+			// 
+			this.txtCost.Location = new System.Drawing.Point(567, 56);
+			this.txtCost.Name = "txtCost";
+			this.txtCost.Size = new System.Drawing.Size(100, 20);
+			this.txtCost.TabIndex = 8;
+			// 
+			// txtPrice
+			// 
+			this.txtPrice.Location = new System.Drawing.Point(567, 82);
+			this.txtPrice.Name = "txtPrice";
+			this.txtPrice.Size = new System.Drawing.Size(100, 20);
+			this.txtPrice.TabIndex = 9;
+			// 
+			// label5
+			// 
+			this.label5.AutoSize = true;
+			this.label5.ForeColor = System.Drawing.Color.White;
+			this.label5.Location = new System.Drawing.Point(533, 59);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(28, 13);
+			this.label5.TabIndex = 10;
+			this.label5.Text = "Cost";
+			this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// label6
+			// 
+			this.label6.AutoSize = true;
+			this.label6.ForeColor = System.Drawing.Color.White;
+			this.label6.Location = new System.Drawing.Point(533, 85);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(31, 13);
+			this.label6.TabIndex = 11;
+			this.label6.Text = "Price";
+			this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// ckBoxIsListed
+			// 
+			this.ckBoxIsListed.AutoSize = true;
+			this.ckBoxIsListed.ForeColor = System.Drawing.Color.White;
+			this.ckBoxIsListed.Location = new System.Drawing.Point(558, 146);
+			this.ckBoxIsListed.Name = "ckBoxIsListed";
+			this.ckBoxIsListed.Size = new System.Drawing.Size(109, 17);
+			this.ckBoxIsListed.TabIndex = 12;
+			this.ckBoxIsListed.Text = "Is Listed in Guild?";
+			this.ckBoxIsListed.UseVisualStyleBackColor = true;
+			// 
+			// btnAdd
+			// 
+			this.btnAdd.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+			this.btnAdd.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+			this.btnAdd.FlatAppearance.BorderSize = 0;
+			this.btnAdd.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.btnAdd.ForeColor = System.Drawing.Color.Black;
+			this.btnAdd.Location = new System.Drawing.Point(592, 108);
+			this.btnAdd.Name = "btnAdd";
+			this.btnAdd.Size = new System.Drawing.Size(75, 23);
+			this.btnAdd.TabIndex = 13;
+			this.btnAdd.Text = "Add";
+			this.btnAdd.UseVisualStyleBackColor = false;
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(8, 25);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(227, 20);
+			this.textBox1.TabIndex = 14;
+			this.textBox1.Text = "Enter Items to Search";
+			// 
+			// btnSearch
+			// 
+			this.btnSearch.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+			this.btnSearch.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+			this.btnSearch.FlatAppearance.BorderSize = 0;
+			this.btnSearch.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.btnSearch.Location = new System.Drawing.Point(241, 23);
+			this.btnSearch.Name = "btnSearch";
+			this.btnSearch.Size = new System.Drawing.Size(75, 23);
+			this.btnSearch.TabIndex = 15;
+			this.btnSearch.Text = "Search";
+			this.btnSearch.UseVisualStyleBackColor = false;
+			// 
+			// comboBox1
+			// 
+			this.comboBox1.FormattingEnabled = true;
+			this.comboBox1.Location = new System.Drawing.Point(325, 25);
+			this.comboBox1.Name = "comboBox1";
+			this.comboBox1.Size = new System.Drawing.Size(342, 21);
+			this.comboBox1.TabIndex = 16;
+			this.comboBox1.Text = "Enter Item Name";
+			// 
+			// btnAll
+			// 
+			this.btnAll.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+			this.btnAll.FlatAppearance.BorderSize = 0;
+			this.btnAll.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.btnAll.Location = new System.Drawing.Point(8, 63);
+			this.btnAll.Name = "btnAll";
+			this.btnAll.Size = new System.Drawing.Size(75, 23);
+			this.btnAll.TabIndex = 18;
+			this.btnAll.Text = "All";
+			this.btnAll.UseVisualStyleBackColor = false;
+			// 
+			// btnNotListed
+			// 
+			this.btnNotListed.BackColor = System.Drawing.Color.Navy;
+			this.btnNotListed.FlatAppearance.BorderSize = 0;
+			this.btnNotListed.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.btnNotListed.ForeColor = System.Drawing.Color.White;
+			this.btnNotListed.Location = new System.Drawing.Point(241, 63);
+			this.btnNotListed.Name = "btnNotListed";
+			this.btnNotListed.Size = new System.Drawing.Size(75, 23);
+			this.btnNotListed.TabIndex = 19;
+			this.btnNotListed.Text = "Not Listed";
+			this.btnNotListed.UseVisualStyleBackColor = false;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+			this.ClientSize = new System.Drawing.Size(677, 328);
+			this.Controls.Add(this.btnNotListed);
+			this.Controls.Add(this.btnAll);
+			this.Controls.Add(this.comboBox1);
+			this.Controls.Add(this.btnSearch);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.btnAdd);
+			this.Controls.Add(this.ckBoxIsListed);
+			this.Controls.Add(this.label6);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.txtPrice);
+			this.Controls.Add(this.txtCost);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.picBoxItemPrice);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.picBoxItemCost);
+			this.Controls.Add(this.listBxItems);
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.Name = "Form1";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "ESO Merchant";
+			((System.ComponentModel.ISupportInitialize)(this.picBoxItemCost)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.picBoxItemPrice)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.ListBox listBxItems;
+		private System.Windows.Forms.PictureBox picBoxItemCost;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.PictureBox picBoxItemPrice;
+		private System.Windows.Forms.TextBox txtCost;
+		private System.Windows.Forms.TextBox txtPrice;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.Label label6;
+		private System.Windows.Forms.CheckBox ckBoxIsListed;
+		private System.Windows.Forms.Button btnAdd;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Button btnSearch;
+		private System.Windows.Forms.ComboBox comboBox1;
+		private System.Windows.Forms.Button btnAll;
+		private System.Windows.Forms.Button btnNotListed;
+	}
+}
+
