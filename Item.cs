468a3a30d47cb8b87ec13bca9815b7cd8fa36c78
James Keith Buchanan <jamesbuchanan007@gmail.com>
2021-02-06 12:48:43 -0600

    Implement builder pattern

--- Item.cs
+++ Item.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.CodeDom;
 using System.Collections.Generic;
+using System.Drawing.Text;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -9,49 +11,61 @@ using System.Windows.Forms.VisualStyles;
 
 namespace ESO_Merchant
 {
-	public class Item
-	{
-		private int ItemID { get; set; }
-		private string Name { get; set; }
-		private decimal Cost { get;set }
-		private decimal Price { get;set }
-		private string Image1 { get; set; }
-		private string Image2 { get; set; }
+    public class Item
+    {
+        private int ItemID { get; set; }
+        private string Name { get; set; }
+        private decimal Cost { get; set }
+        private decimal Price { get; set }
+        private string Image1 { get; set; }
+        private string Image2 { get; set; }
 
-		private Item(string name, decimal cost, decimal price, string image1, string image2)
-		{
-			this.Name = name;
-			this.Cost = cost;
-			this.Price = price;
-			this.Image1 = image1;
-			this.Image2 = image2;
-		}
+        private Item(string name, decimal cost, decimal price, string image1, string image2)
+        {
+            this.Name = name;
+            this.Cost = cost;
+            this.Price = price;
+            this.Image1 = image1;
+            this.Image2 = image2;
+        }
 
-		public Item BuildItem(string name, string cost, string price, string image1, string image2)
-		{
-			//Build item ID
-			// set image IDs
-			var checkedName = string.Empty;
-			if (!string.IsNullOrEmpty(name) || name.Length > 100)
-				checkedName = name;
-			else throw new Exception("Name is Null or Name is too long");
+        public class BuildItem
+        {
+            private Item item;
+            private string checkedName;
+            private decimal checkedCost;
+            private decimal checkedPrice;
+            private string checkedImage1;
+            private string checkedImage2;
+            public BuildItem(string name, string cost, string price, string image1, string image2)
+            {
+                if (!string.IsNullOrEmpty(name) || name.Length < 100)
+                    checkedName = name;
+                else throw new Exception("Name is Null or Name is too long");
 
-			var isCostValid = decimal.TryParse(cost, out var checkedCost);
-			if(!isCostValid) throw new Exception("Cost is not a number");
+                var isCostValid = decimal.TryParse(cost, out checkedCost);
+                if (!isCostValid) throw new Exception("Cost is not a number");
 
-			var isPriceValid = decimal.TryParse(price, out var checkedPrice);
-			if (!isPriceValid) throw new Exception("Price is not a number");
+                var isPriceValid = decimal.TryParse(price, out checkedPrice);
+                if (!isPriceValid) throw new Exception("Price is not a number");
 
-			var checkedImage1 = string.Empty;
-			if (!string.IsNullOrEmpty(image1) || image1.Length != 14) checkedImage1 = image1;
-			else throw new Exception("Something wrong with Image 1");
+                if (!string.IsNullOrEmpty(image1) || image1.Length != 14) checkedImage1 = image1;
+                else throw new Exception("Something wrong with Image 1");
 
-			var checkedImage2 = string.Empty;
-			if (!string.IsNullOrEmpty(image2) || image2.Length != 14) checkedImage2 = image2;
-			else throw new Exception("Something wrong with Image 1");
+                if (!string.IsNullOrEmpty(image2) || image2.Length != 14) checkedImage2 = image2;
+                else throw new Exception("Something wrong with Image 1");
 
-			return new Item(checkedName, checkedCost, checkedPrice, checkedImage1, checkedImage2);
-		}
-		
-	}
+                item = new Item(checkedName, checkedCost, checkedPrice, checkedImage1, checkedImage2);
+            }
+
+            public Item Build()
+            {
+                Item newItem = null;
+                if (item != null) newItem = item;
+                item = null;    // Destroy Builder Item
+                return newItem;
+            }
+        }
+
+    }
 }
